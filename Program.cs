450cff026b6289df33d74394d58c0ec4fd2ca71e
Ron-1993 <enjoylifesteve5@gmail.com>
2021-10-07 16:06:48 +0800

    練習提交

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class HelloWorld
+{
+    static void Main()
+    {
+        string max, miniMax,name;
+        int money;
+        var ( A, B) = ("momther", "Fuck");
+        string[,] language ={
+            {"C#", "123", "C++"},
+            {"java","javascript","456"},
+        };
+
+        
+        
+        miniMax = "Hello World! I'm FunFun";
+        max = "Who are you?";
+        Console.WriteLine(miniMax);
+        Console.Write(max);
+        name = Console.ReadLine();
+        Console.WriteLine($"Hello {name} How much money do you have?");
+        money = int.Parse(Console.ReadLine());;
+        
+        Console.WriteLine($"Wow {A} {B}! you have {money} doller! you are rich!");
+        Console.WriteLine($"there {language[1,2]} in this pargram");
+        Console.WriteLine(language.Length);
+    }
+}
+
+
+
